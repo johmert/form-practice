328cc9b858e1d1d83b6a79d3faf6ce60f471800a
johmert <johmert@users.noreply.github.com>
2022-01-09 03:25:12 -0800

    submissions

--- src/SubscriberForm.js
+++ src/SubscriberForm.js
@@ -1,14 +1,28 @@
+import React, {useState} from "react";
+
 function SubscriberFrom() {
+    const [name, setName] = useState("");
+    const handleNameChange = event => setName(event.target.value);
+
+    const [email, setEmail] = useState("");
+    const handleEmailChange = event => setEmail(event.target.value);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(`Form Submitted! ${name} : ${email}`)
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="name">
                 Enter Your Name:
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" onChange={handleNameChange} value={name} />
             </label>
             <label htmlFor="email">
                 Enter Your Email:
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" onChange={handleEmailChange} value={email} />
             </label>
+            <button type="submit">Submit</button>
         </form>
     );
 }
